40e0441ab854b5161b43d3a5b188ec879cb5b999
NitayMalka <nitaai.maalka@gmail.com>
2026-06-12 08:57:34 +0300

    Rename icon files to v2 to bypass Windows cache and update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function createWindow() {
     minWidth: 1000,
     minHeight: 650,
     title: "Photo Nitis - Premium Photo Organizer",
-    icon: path.join(__dirname, 'app_icon.png'),
+    icon: path.join(__dirname, 'app_icon_v2.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
